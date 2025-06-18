62eacd86184de23e36f5b6f46e3874a54d840c14
Shyam Chinta <39310462+DarXSouls@users.noreply.github.com>
2025-06-18 21:26:52 +0530

    Update get_accel_for_active.f90

--- nbd_1/get_accel_for_active.f90
+++ nbd_1/get_accel_for_active.f90
@@ -1,6 +1,5 @@
 subroutine get_accel_for_active(N, N_act, i_active, x, y, z, vx, vy, vz, m, soft, ax, ay, az, axdot, aydot, azdot, radii)
 !
-! pcc - 18/10/2020
 !
 ! This subroutine returns the accelations, based on the current position,
 ! velocity, mass, and softening.
